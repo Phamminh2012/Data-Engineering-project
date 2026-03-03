805e2a4d02152ed98364fd2f9104f3daddd3f75f
zhd1987e <zhanghaodong101@outlook.com>
2026-03-03 22:58:34 +0800

    Minh has fixed the DAG driver

--- dags/driver_dag.py
+++ dags/driver_dag.py
@@ -1,6 +1,7 @@
 # Import Airflow here
 import datetime
 
+<<<<<<< HEAD
 from airflow import DAG
 from airflow.operators import PythonOperator
 from airflow.operators import EmptyOperator
@@ -12,3 +13,49 @@ with DAG(
     start_date=datetime.datetime(2010, 1, 1),
 ) as dag:
     start = EmptyOperator(task_id="to_be_filled")
+=======
+# Define DAG
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from publicapi_import import get_from_job_portal  as data_1
+from rapidapi_import import fetch_jsearch_jobs as data_2
+
+
+with DAG(
+    dag_id='Testing_airflow_first_time',
+    description='Testing call API',
+    start_date=datetime(2026, 1, 1), # YYYY , MM, DD
+    
+    # --- SCHEDULE EXAMPLES ---
+    # schedule='@daily',        # Preset: Runs once a day at midnight
+    # schedule='@hourly',       # Preset: Runs at the beginning of every hour
+    # schedule='0 12 * * *',    # Cron expression: Runs exactly at 12:00 PM every day
+    schedule=None,
+    
+    # --- CATCHUP EXPLANATION ---
+    # If catchup=True: Airflow looks at your start_date and automatically runs 
+    # every missed scheduled interval between that date and right now (called "backfilling").
+    # If catchup=False: It ignores the past and only runs from the current date onward.
+    catchup=False,
+    
+    tags=['tutorial_4'],
+) as dag:
+
+    get_rapid_data = PythonOperator(
+        task_id='get_rapid_data',
+        python_callable=data_2,
+    )
+
+    get_public_data = PythonOperator(
+        task_id='get_public_data',
+        python_callable=data_1,
+    )
+
+   
+    
+    # 4. Set dependencies
+    # The '>>' operator tells Airflow the order of execution. 
+    # Here, hello_task must finish successfully before world_task is allowed to start.
+  
+>>>>>>> 2bfaf91b0c2171d019f232f0a28a31c8a9c296ae
