38463c3893a31002911cc72a3ccccca3ee648395
zhd1987e <zhanghaodong101@outlook.com>
2026-03-03 18:58:15 +0800

    Initiate LocalHost and BaseLine Driver DAG

--- dags/driver_dag.py
+++ dags/driver_dag.py
@@ -1,3 +1,14 @@
 # Import Airflow here
+import datetime
+
+from airflow import DAG
+from airflow.operators import PythonOperator
+from airflow.operators import EmptyOperator
 
 # Define DAG
+
+with DAG(
+    dag_id="Driver DAG",
+    start_date=datetime.datetime(2010, 1, 1),
+) as dag:
+    start = EmptyOperator(task_id="to_be_filled")
