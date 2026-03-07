d6a4f6009b9871f2bc1fa995d0a9b81f8a41c89d
Pham Minh <pm0592625@gmail.com>
2026-03-07 20:40:24 +0800

    only rapid and adzuna

--- dags/driver_dag.py
+++ dags/driver_dag.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-from reed_import import get_from_reed  as data_1
+
 from rapidapi_import import fetch_jsearch_jobs as data_2
 from azuna_import import adzuna_import as data_3
 from upload_data import upload as upload_data
@@ -34,25 +34,22 @@ with DAG(
         task_id='get_rapid_data',
         python_callable=data_2,
     )
-   
-    get_public_data = PythonOperator(
-        task_id='get_reed_data',
-        python_callable=data_1,
-    )
-    
     get_telegram_data = PythonOperator(
        task_id='get_azuna_data',
        python_callable=data_3
-   )
+    )
    
+
+   
+    '''
     upload_data_gcs = PythonOperator(
         task_id='upload_to_gcs',
         python_callable=upload_data
     )
-    
+    '''
     # 4. Set dependencies
     # The '>>' operator tells Airflow the order of execution. 
     # Here, hello_task must finish successfully before world_task is allowed to start.
-    get_public_data >> upload_data_gcs
+   
 
   
