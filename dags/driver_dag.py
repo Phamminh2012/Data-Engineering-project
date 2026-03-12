d78c1bb84d2c9f3c0e0be7c581fc1b737cbb2077
zhd1987e <zhanghaodong101@outlook.com>
2026-03-13 00:21:56 +0800

    Using the new `dag` and `task` decorators

--- dags/driver_dag.py
+++ dags/driver_dag.py
@@ -1,69 +1,34 @@
-# Import Airflow here
+from airflow.sdk import dag, task
+import datetime
+from scraping import *
+from upload_data import upload
 
-# Define DAG
-from datetime import datetime
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+@dag("job_scraper", schedule= "@daily", start_date=datetime.datetime(2026, 1, 1))
+def the_driver():
 
-
-from rapidapi_import import fetch_jsearch_jobs as data_2
-from azuna_import import adzuna_import as data_3
-from mcf_scrap import mcf_scrape as data_1
-from upload_data import upload as upload_data
-
-
-with DAG(
-    dag_id='JobSearchAndAnalysis',
-    description='IS3107 group project on job analytics',
-    start_date=datetime(2026, 1, 1), # YYYY , MM, DD
+    @task(task_id="MCF-Scrape")
+    def mcf():
+        return mcf_scrape("software engineer", 50)
     
-    # --- SCHEDULE EXAMPLES ---
-    # schedule='@daily',        # Preset: Runs once a day at midnight
-    # schedule='@hourly',       # Preset: Runs at the beginning of every hour
-    # schedule='0 12 * * *',    # Cron expression: Runs exactly at 12:00 PM every day
-    schedule='@daily',
+    @task(task_id="Adzuna-Scrape")
+    def adzuna():
+        return adzuna_import()
     
-    # --- CATCHUP EXPLANATION ---
-    # If catchup=True: Airflow looks at your start_date and automatically runs 
-    # every missed scheduled interval between that date and right now (called "backfilling").
-    # If catchup=False: It ignores the past and only runs from the current date onward.
-    catchup=False,
-    
-    tags=['project'],
-)as dag:
-    
-    get_rapid_data = PythonOperator(
-        task_id='get_rapid_data',
-        python_callable=data_2,
-    )
-    get_adzuna_data = PythonOperator(
-       task_id='get_azuna_data',
-       python_callable=data_3
-    )
-    
-    get_mcf_data = PythonOperator(
-       task_id='get_mcf_data',
-       python_callable=data_1,
-       op_kwargs={
-        "keywords": "software engineer",
-        "limit": 50
-        }
-    )
+    @task(task_id="JSearch-Scrape")
+    def jSearch():
+        return fetch_jsearch_jobs(query = "software engineer in singapore")
 
-   
-    
-    upload_mongo = PythonOperator(
-        task_id='upload_to_mongo',
-        python_callable=upload_data
-    )
-    
-    get_rapid_data >> upload_mongo 
-    get_adzuna_data >> upload_mongo 
-    get_mcf_data >> upload_mongo
-    # 4. Set dependencies
-    # The '>>' operator tells Airflow the order of execution. 
-    # Here, hello_task must finish successfully before world_task is allowed to start.
+    @task(task_id="GOV-SG-Scrape")
+    def govSearch():
+        return fetch_gov_sg_listings()
 
-   
+    m = mcf()
+    a = adzuna()
+    j = jSearch()
+    g = govSearch()
+    raw_db_name = "raw_api_result"
 
-  
+    upload(raw_db_name,"mcf", m)
+    upload(raw_db_name,"adzuma", a)
+    upload(raw_db_name, "jsearch", j)
+    upload(raw_db_name, "govsearch", g)
