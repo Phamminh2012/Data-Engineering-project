d5a9ba53458dbb03fc0aa8e9f2ea2ab06cc78eed
ZHD1987E <zhanghaodong101@outlook.com>
2026-03-13 13:53:05 +0800

    ETL code for PyMongo, for Minh's use

--- transformationfile.py
+++ transformationfile.py
@@ -10,7 +10,8 @@ result = client['raw_api_result']['mcf'].aggregate([
             'title': 1, 
             'description': 1, 
             'postedCompany': 1, 
-            'metadata': 1
+            'metadata': 1, 
+            'skills': 1
         }
     }, {
         '$set': {
@@ -20,6 +21,7 @@ result = client['raw_api_result']['mcf'].aggregate([
                 }
             }, 
             'company': '$postedCompany.name', 
+            'skills': '$skills.skill', 
             'source': 'mcf'
         }
     }, {
@@ -69,3 +71,38 @@ result = client['raw_api_result']['adzuma'].aggregate([
     }
 ])
 
+######
+
+from pymongo import MongoClient
+
+# Requires the PyMongo package.
+# https://api.mongodb.com/python/current
+
+client = MongoClient('mongodb://localhost:27017/')
+result = client['raw_api_result']['jsearch'].aggregate([
+    {
+        '$project': {
+            'job_title': 1, 
+            'job_description': 1, 
+            'eployer_name': 1, 
+            'job_posted_at_datetime_utc': 1
+        }
+    }, {
+        '$set': {
+            'created': {
+                '$dateFromString': {
+                    'dateString': '$job_posted_at_datetime_utc'
+                }
+            }
+        }
+    }, {
+        '$project': {
+            'job_posted_at_datetime_utc': 0
+        }
+    }, {
+        '$out': {
+            'db': 'transformed', 
+            'coll': 'transformed'
+        }
+    }
+])
