ba51b36a3be898f6e4f436b3fdbe300fbfdf698f
ZHD1987E <zhanghaodong101@outlook.com>
2026-03-15 12:00:37 +0800

    Changed search queries and removed Adzuma refeences from driver DAG

--- dags/driver_dag.py
+++ dags/driver_dag.py
@@ -9,11 +9,7 @@ def the_driver():
 
     @task(task_id="MCF-Scrape")
     def mcf():
-        return mcf_scrape("it job", 50)
-    
-    @task(task_id="Adzuna-Scrape")
-    def adzuna():
-        return adzuna_import("it job")
+        return mcf_scrape("software engineer", 50)
     
     @task(task_id="JSearch-Scrape")
     def jSearch():
@@ -26,26 +22,19 @@ def the_driver():
     @task(task_id="Transform-MCF-Mongo")
     def transformMCFOnMongo(something):
         return transformMCF(something)
-
-    @task(task_id="Transform-Adzuma-Mongo")
-    def transformAdzumaOnMongo(something):
-        return transformAdzuma(something)
     
     @task(task_id="Transform-JSEARCH-Mongo")
     def transformJSEARCHOnMongo(something):
         return transformJSearch(something)
 
     m = mcf()
-    a = adzuna()
     j = jSearch()
     raw_db_name = "raw_api_result"
 
     upload_raw(raw_db_name,"mcf", m)
-    upload_raw(raw_db_name,"adzuma", a)
     upload_raw(raw_db_name, "jsearch", j)
 
     transformMCFOnMongo(m)
-    transformAdzumaOnMongo(a)
     transformJSEARCHOnMongo(j)
 
 the_driver()
